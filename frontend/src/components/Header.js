cb1524e5170ec31b1f4a42bcceee27760f9969d9
Akshit Kumar <akshitkumar450@gmail.com>
2022-02-19 00:45:25 +0530

    frontend with pages

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,40 @@
+import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
+import { Link } from "react-router-dom";
+
+function Header() {
+  const user = false;
+
+  const onLogout = () => {};
+
+  return (
+    <header className="header">
+      <div className="logo">
+        <Link to="/">GoalSetter</Link>
+      </div>
+      <ul>
+        {user ? (
+          <li>
+            <button className="btn" onClick={onLogout}>
+              <FaSignOutAlt /> Logout
+            </button>
+          </li>
+        ) : (
+          <>
+            <li>
+              <Link to="/login">
+                <FaSignInAlt /> Login
+              </Link>
+            </li>
+            <li>
+              <Link to="/register">
+                <FaUser /> Register
+              </Link>
+            </li>
+          </>
+        )}
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
